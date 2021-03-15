6091d1b74e1af24380966adc5c95ef2112291802
fleger42 <florianleger91120@gmail.com>
2021-03-15 01:09:34 +0100

    update

---
+++ push_swap_tester.sh
@@ -0,0 +1,90 @@
+RESET="\033[0m"
+BLACK="\033[30m"
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+MAGENTA="\033[35m"
+CYAN="\033[36m"
+WHITE="\033[37m"
+
+BOLDBLACK="\033[1m\033[30m"
+BOLDRED="\033[1m\033[31m"
+BOLDGREEN="\033[1m\033[32m"
+BOLDYELLOW="\033[1m\033[33m"
+BOLDBLUE="\033[1m\033[34m"
+BOLDMAGENTA="\033[1m\033[35m"
+BOLDCYAN="\033[1m\033[36m"
+BOLDWHITE="\033[1m\033[37m"
+
+if [[ $# -ne 2 ]]
+then
+    printf $BOLDRED"Wrong number of elements !\nUsage = \"bash push_swap_tester.sh [start][end]\"$RESET\n"
+    exit 1
+fi
+if [[ "$1" -ge "$2" ]] || [[ "$1" -lt 0 ]] || [[ "$2" -lt 0 ]]
+then
+    printf $BOLDRED"Wrong range of elements !$RESET\n"
+    exit 1
+fi
+printf $BOLDYELLOW"Compiling your push_swap and checker...$RESET\n"
+make -C ../ > /dev/null
+cp ../push_swap ../checker .
+chmod 755 push_swap
+chmod 755 checker
+printf $BOLDYELLOW"Compiling succesfull !$RESET\n"
+sleep 1
+let "NBR_TEST=$2 - $1"
+printf $BOLDYELLOW"Begin test of $BOLDRED$NBR_TEST$BOLDYELLOW random list from $BOLDRED$1$BOLDYELLOW to $BOLDRED$2$BOLDYELLOW!$RESET\n"
+function basetest()
+{
+	TOTAL=$WHITE
+	NBR=0
+	NBR_ERROR=0
+	for NBR in `seq $1 $2`;
+	do
+		LIST=$(perl -e "use List::Util 'shuffle'; my @out = (shuffle 0..$NBR)[0..$NBR]; print \"@out\"")
+		set -v
+		ARG=${LIST[@]}; ./push_swap $ARG | ./checker $ARG > /dev/null
+		RET=$?
+		if [[ $RET != 0 ]] 
+		then
+			printf $BOLDRED"Fail$RESET with size $TOTAL$NBR/$2$RESET\n"
+			echo "LIST = "${LIST[@]}
+			let "NBR_ERROR=NBR_ERROR+1"
+		else
+			printf $BOLDGREEN"Success$RESET with size $TOTAL$NBR/$2$RESET\n"
+			echo "LIST = "${LIST[@]}
+			echo 
+		fi
+		let "NBR=NBR+1"
+		if [[ $NBR = 100 ]] 
+		then
+			TOTAL=$GREEN
+		fi
+		if [[ $NBR = 200 ]] 
+		then
+			TOTAL=$BLUE
+		fi
+		if [[ $NBR = 300 ]] 
+		then
+			TOTAL=$MAGENTA
+		fi
+		if [[ $NBR = 400 ]] 
+		then
+			TOTAL=$YELLOW
+		fi
+		if [[ $NBR = 500 ]] 
+		then
+			TOTAL=$RED
+		fi
+	done
+	if [[ $NBR_ERROR != 0 ]]
+	then
+		printf $BOLDRED"Failed !$RESET You got $BOLDRED$NBR_ERROR$RESET error in $BLUENBR_TEST$RESET test\n"
+	else
+		printf $BOLDGREEN"Well done !$RESET\n"
+	fi
+}
+
+basetest $1 $2
